2fbf826f2f374836d88b7e2d99893ef31613ab9a
HumbertoArellano <humbertoyham25@gmail.com>
2023-09-12 10:05:46 -0700

    Update main with Liskov

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,24 @@ import com.arellano.designpatterns.openclose.Dog;
 import com.arellano.presentation.Presentation;
 import com.arellano.presentation.PresentationOpenClose;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<IFelino> listaDeFelinos = new ArrayList<>();
+
         Jaguar jaguar = new Jaguar(23, "Sabana");
         IFelino jaguar2 = new Jaguar(23, "Sabana");
         IFelinoSalvaje jaguar3 = new Jaguar(23, "Sabana");
+
+        listaDeFelinos.add(jaguar);
+        listaDeFelinos.add(jaguar2);
+        listaDeFelinos.add(jaguar3);
+
+        for(IFelino jaguarAll: listaDeFelinos){
+            System.out.println(jaguarAll);
+        }
+
     }
 }
